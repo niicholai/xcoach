0b5f7d15678786cecb086f4a8e32c4db73b0da7b
Niicholai <derek.h.folds@gmail.com>
2026-03-05 10:31:53 -0600

    Fix baseUrl path and site title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Docs With Friends",
+    pageTitle: "House Black Member Growth",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "docs-with-friends.houseblack.org",
+    baseUrl: "xcoach.houseblack.org",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
